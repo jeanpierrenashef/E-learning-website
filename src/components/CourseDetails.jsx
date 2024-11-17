ca2bee02c5d8f5d680b811f47b0d9d95786dc198
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-18 02:18:33 +0300

    Update - students can now post private and public comments

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,11 +1,17 @@
     import React, { useState, useEffect } from "react";
     import { useParams } from "react-router-dom";
     import axios from "axios";
+    import "../styles/CourseDetails.css"
 
     const CourseDetails = () => {
-    const { title } = useParams(); // Extract `title` from the URL
+    const { title } = useParams(); 
     const [movieDetails, setMovieDetails] = useState(null);
     const [error, setError] = useState(false);
+    const user_id = localStorage.getItem("user_id");
+    const [comment, setComment] = useState("");
+    useEffect(()=>{
+        console.log(comment)
+    },[comment])
 
     const loadMovieDetails = () => {
         axios
@@ -37,14 +43,49 @@
         return <p>Loading...</p>;
     }
 
-    const { genre, release_year, details } = movieDetails;
+    const { movie_id, genre, release_year, details } = movieDetails;
 
     return (
         <div className="course-details">
-        <h2>{title}</h2>
-        <h3>Genre: {genre}</h3>
-        <h3>Release Year: {release_year}</h3>
-        <p>{details}</p>
+            <h2>{title}</h2>
+            <h3>Genre: {genre}</h3>
+            <h3>Release Year: {release_year}</h3>
+            <p>{details}</p>
+            <div className="comment-section">
+                <input type="text" placeholder="Comment" onChange={(e)=>{
+                    setComment(e.target.validationMessage)
+                }}/>
+                <button onClick={()=>{
+                    const data = new FormData();
+                    data.append("user_id", user_id);
+                    data.append("movie_id", movie_id);
+                    data.append("comment", comment);
+
+                    axios("http://localhost/AI-Movie-Recommender/server-side/addPublicComment_TEST.php",{
+                        method:"POST",
+                        data:data
+                    }).then((response)=>{
+                        console.log("public comment added")
+                    }).catch(()=>{
+                        console.log("posting public comment failed")
+                    })
+                }}>Public</button>
+                <button onClick={()=>{
+                    const data = new FormData();
+                    data.append("user_id", user_id);
+                    data.append("movie_id", movie_id);
+                    data.append("comment", comment);
+
+                    axios("http://localhost/AI-Movie-Recommender/server-side/addPrivateComment_TEST.php",{
+                        method:"POST",
+                        data:data
+                    }).then((response)=>{
+                        console.log("private comment added")
+                    }).catch(()=>{
+                        console.log("posting private comment failed")
+                    })
+                }}>Private</button>
+            </div>
         </div>
     );
     };
