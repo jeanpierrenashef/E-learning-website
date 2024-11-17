22370643fb849aa83d686d4da60ac56b8184feb0
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-18 01:00:45 +0300

    Toggle - allowed student to drop and enroll in courses

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -5,21 +5,26 @@ import "../styles/CourseCard.css";
 const CourseCard = ({ movie }) => {
     const navigate = useNavigate();
     const { movie_id, title, isEnrolled} = movie;
-    const [toggle, setToggle] = useState(false);
+    const [toggle, setToggle] = useState(isEnrolled ? "Drop" : "Enroll");
 
+    const handleToggle = () => {
+        setToggle((prev) => (prev === "Drop" ? "Enroll" : "Drop")); // Toggle between "Drop" and "Enroll"
+    };
 
     return (
-        <div
-        className="course-card"
-        onClick={() => {
-            navigate(`/courses/${title}`);
-        }}
-        >
-            <div className = "course-allign">
-                <h3>{title}</h3>
-                <h3>{isEnrolled ? "true" : "false"}</h3>
-                
+        <div className="course-card">
+            <div className = "course-allign"
+                >
+                <h3 onClick={() => {
+                    navigate(`/courses/${title}`);
+                }}>{title}</h3>
+
+                <button onClick={handleToggle}>
+                    {toggle.toString()}
+                </button>
+
             </div>
+
         </div>
     );
     };
