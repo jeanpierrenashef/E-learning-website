ea44ef168b769145b9f487bc8629d5e2c0b668c0
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-20 17:30:02 +0300

    Update - admin can add instructor

--- src/pagess/Admin.jsx
+++ src/pagess/Admin.jsx
@@ -8,7 +8,11 @@ import AdminCourseCard from "../components/AdminCourseCard";
 const Admin = () => {
     const [users, setUsers] = useState([]);
     const [courses, setCourses] = useState([]);
-    const [addCourse, setAddCourse] = useState("")
+    const [addCourse, setAddCourse] = useState("");
+    const [addInstructors, setAddInstructors] = useState({
+        username:"",
+        password:""
+    })
 
     const loadAllUsers = () => {
         axios("http://localhost/AI-Movie-Recommender/server-side/getAllUsers_TEST.php",{
@@ -50,33 +54,74 @@ const Admin = () => {
             console.log("error adding course")
         })
     }
+    
+    const handleAddingInstructor = (instructorsName) => {
+        const data = new FormData();
+        data.append("username", instructorsName.username);
+        data.append("password", instructorsName.password)
+        axios("http://localhost/AI-Movie-Recommender/server-side/addInstructors_TEST.php",{
+            method:"POST",
+            data:data
+    }).then(()=>{
+        setAddInstructors({
+            username:"",
+            password:""
+        })
+        loadAllUsers();
+    }).catch(()=>{
+        console.log("failed to add isntructor account")
+    })}
 
 
     return (
         <div className="admin">
-            
-            <div>
-                <h2>All Users</h2>
-                {users.map((u)=>(
-                    <BanUserCard user={u} key={u.user_id} />
-                ))}
+            <div className="admin-display">
+                <div>
+                    <h2>All Users</h2>
+                    {users.map((u)=>(
+                        <BanUserCard user={u} key={u.user_id} />
+                    ))}
+                </div>
+                <div>
+                    <h2>All Courses</h2>
+                    {courses.map((u)=>(
+                        <AdminCourseCard course={u} key={u.movie_id} loadCourses={loadCourses}/>
+                    ))}
+                    <h4>Add Course:</h4>
+                    <input
+                        type="text"
+                        placeholder="Add Course Name"
+                        value={addCourse}
+                        onChange={(e) => setAddCourse(e.target.value)}
+                    />
+                    <button onClick={() => handleAddingCourse(addCourse)}>Add Course</button>
+                </div>
             </div>
             <div>
-                <h2>All Courses</h2>
-                {courses.map((u)=>(
-                    <AdminCourseCard course={u} key={u.movie_id} loadCourses={loadCourses}/>
-                ))}
-                <h4>Add Course:</h4>
-                <input
-                    type="text"
-                    placeholder="Add Course Name"
-                    value={addCourse}
-                    onChange={(e) => setAddCourse(e.target.value)}
-                />
-                <button onClick={() => handleAddingCourse(addCourse)}>Add Course</button>
+                <div className="admin-add-instructor">
+                    <h2>Create Instructor</h2>
+                    <input type="text" placeholder="Instructor's Name" value={addInstructors.username} onChange={(e)=>{
+                        setAddInstructors({
+                            ...addInstructors,
+                            username: e.target.value,
+                        })
+                    }}/>
+                    <input type="password" placeholder="Instructor's Password" value={addInstructors.password} onChange={(e)=>{
+                        setAddInstructors({
+                            ...addInstructors,
+                            password: e.target.value,
+                        })
+                    }}/>
+                    <button onClick={()=>{
+                        handleAddingInstructor(addInstructors)
+                    }}>Add Instructor
+                    </button>
+                </div>
             </div>
+
         </div>
     );
 };
+
 
 export default Admin;
