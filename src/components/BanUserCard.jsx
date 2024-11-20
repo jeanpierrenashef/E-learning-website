8934a64ef87fc778e4efcbacb54d05a485396195
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-20 03:03:20 +0300

    Update - more bug fixes

--- src/components/BanUserCard.jsx
+++ src/components/BanUserCard.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../styles/Admin.css";
 
-const BanUserCard = ({ user }) => {
+
+const BanUserCard = ({ user, setUsers }) => {
     const { user_id, username, user_type, banned } = user;
     const [toggle, setToggle] = useState(banned === 1 ? "Unban" : "Ban");
 
-    const handleToggle = () => {
+    const handleToggle =  () => {
         const data = new FormData();
         data.append("user_id", user_id);
         data.append("isBanned", banned === 1 ? 0 : 1); 
+        axios("http://localhost/AI-Movie-Recommender/server-side/toggleBanUser_TEST.php", {
+                method: "POST",
+                data: data,
+            }).then(()=>{
+                setToggle(toggle === "Ban" ? "Unban" : "Ban");
+            }).catch (()=> {
+                console.error("Error toggling ban status");
+            })    
+        } 
 
-        axios("http://localhost/AI-Movie-Recommender/server-side/toggleBanUser.php", {
-            method: "POST",
-            data: data,
-        }).then(()=>{
-            setToggle(toggle === "Ban" ? "Unban" : "Ban");
-        }).catch (()=>{
-            console.error("Error toggling ban status");
-        })
-    };
 
     return (
         <div className="ban-section">
@@ -29,7 +29,7 @@ const BanUserCard = ({ user }) => {
             <button onClick=
             {handleToggle}>
                 {toggle}
-            </button>
+                </button>
         </div>
     );
 };
--- src/pagess/Admin.jsx
+++ src/pagess/Admin.jsx
@@ -7,19 +7,15 @@ import BanUserCard from "../components/BanUserCard";
 const Admin = () => {
     const [users, setUsers] = useState([]);
 
-    const loadAllUsers = async () => {
-        const response = await axios(
-            "http://localhost/AI-Movie-Recommender/server-side/getAllUsers_TEST.php",
-            {
+    const loadAllUsers = () => {
+        axios("http://localhost/AI-Movie-Recommender/server-side/getAllUsers_TEST.php",{
                 method: "GET",
-            }
-        );
-
-        // const usersWithToggle = response.data.response.map((user) => ({
-        //     ...user,
-        //     toggle: user.banned === 1 ? "Unban" : "Ban", 
-        // }));
-        // setUsers(usersWithToggle);
+            }).then((response)=>{
+                setUsers(response.data.response);
+            }).catch(()=>{
+                console.log("not fetching users correctly")
+            })
+        
     };
 
     useEffect(() => {
@@ -28,10 +24,12 @@ const Admin = () => {
 
     return (
         <div className="admin">
-            <h1>All Users</h1>
-            {users.map((u)=>(
-                <BanUserCard user={u} key={u.user_id} />
-            ))}
+            <div>
+                <h1>All Users</h1>
+                {users.map((u)=>(
+                    <BanUserCard user={u} key={u.user_id} />
+                ))}
+            </div>
         </div>
     );
 };
